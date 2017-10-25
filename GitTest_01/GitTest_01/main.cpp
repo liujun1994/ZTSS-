d2c3f95388cbd7249f0dbd1a722a6e8db87a9512
liujun1994 <523274522@qq.com>
2017-10-25 14:40:27 +0800

    初始化一个空的项目

---
+++ GitTest_01/GitTest_01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitTest_01
+//
+//  Created by Admin on 17/10/25.
+//  Copyright © 2017年 Admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
